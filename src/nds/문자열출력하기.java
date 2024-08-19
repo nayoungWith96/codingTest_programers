cb8990f15e8e7397c865aaf68e1f6b504ece7ecf
남동석 <ehdtka456@naver.com>
2024-08-20 03:28:34 +0900

    first commit

---
+++ src/nds/문자열출력하기.java
@@ -0,0 +1,26 @@
+package nds;
+
+import java.util.Scanner; /**
+ * packageName    : nds
+ * fileName       : 문자열
+ * author         : ehdtka
+ * date           : 2024-08-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-20        ehdtka       최초 생성
+ */
+public class 문자열출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+
+}
+
+
+
