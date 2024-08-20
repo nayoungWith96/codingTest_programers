879f754d9908620e01018a6445323962aa41b241
Nayoung77 <107744818+nayoung77@users.noreply.github.com>
2024-08-21 00:19:29 +0900

    commit 0821

---
+++ src/YNY/a와b출력하기.java
@@ -0,0 +1,26 @@
+package YNY;
+
+import java.util.Scanner;
+
+/**
+ * packageName    : nds
+ * fileName       : a와b출력하기
+ * author         : ehdtka
+ * date           : 2024-08-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-20        ehdtka       최초 생성
+ */
+public class a와b출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println ("a = " + a);
+        System.out.println ("b = " + b);
+
+    }
+}
---
+++ src/YNY/문자열반복해서출력하기.java
@@ -0,0 +1,25 @@
+package YNY;
+
+import java.util.Scanner;
+
+/**
+ * packageName    : nds
+ * fileName       : 문자열반복해서출력하기
+ * author         : ehdtka
+ * date           : 2024-08-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-20        ehdtka       최초 생성
+ */
+public class 문자열반복해서출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(str);
+        }
+    }
+}
