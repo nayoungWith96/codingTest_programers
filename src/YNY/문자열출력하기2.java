57512b81efa3b65c33578541249c2eb46a49996f
Nayoung77 <107744818+nayoung77@users.noreply.github.com>
2024-08-21 00:23:16 +0900

    commit 0821

--- src/YNY/문자열출력하기2.java
+++ src/YNY/문자열출력하기2.java
@@ -1,10 +1,8 @@
 package YNY;
 
-import java.util.Scanner;
-
-/**
+import java.util.Scanner; /**
  * packageName    : nds
- * fileName       : 문자열반복해서출력하기
+ * fileName       : 문자열
  * author         : ehdtka
  * date           : 2024-08-20
  * description    :
@@ -13,13 +11,14 @@ import java.util.Scanner;
  * -----------------------------------------------------------
  * 2024-08-20        ehdtka       최초 생성
  */
-public class 문자열반복해서출력하기 {
+public class 문자열출력하기2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            System.out.print(str);
-        }
+        String a = sc.next();
+
+        System.out.println(a);
     }
 }
+
+
+
